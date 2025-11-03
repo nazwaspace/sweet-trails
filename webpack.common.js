2d9bd9f8723886319f49e577457d41b57db17daa
Nazwa Asyifa <nazwaasyifaa05@gmail.com>
2025-11-03 22:13:40 +0700

    Fix service worker path for GitHub Pages

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,9 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/sweet-trails/',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/sweet-trails/'
+    : '/',
     clean: true,
   },
   module: {
